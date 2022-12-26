8526b3c7c89e8c8e8d8f735a26f1c9a13cc76b85
Sebastian Souberbielle <sebastian.souberbielle@mercadolibre.com>
2022-12-26 14:07:38 -0300

    Clase 3 ejs de estrucutras

---
+++ clase3-go/ej1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+)
+
+type Product struct {
+	ID int
+	Name string
+	Price float64
+	Description string
+	Category string
+}
+
+var Products = []Product{
+	{ID: 1, Name: "Monitor", Price: 209.99, Description: "24 pulgadas", Category: "Tecnologia"},
+	{ID: 2, Name: "Teclado RGB", Price: 99.99, Description: "Mecanico RGB", Category: "Tecnologia"},
+}
+
+func (p Product) Save() {
+	Products = append(Products, p)
+}
+
+func (p Product) GetAll() {
+	for _, product := range Products {
+		fmt.Println(product)
+	}
+}
+
+func getById(id int) (Product, error) {
+	for _, product := range Products {
+		if id == product.ID {
+			return product, nil
+		}
+	}
+	return Product{}, errors.New("Product not found.")
+}
+
+func main() {
+	prod := Product{ID: 3, Name: "PC", Price: 499.99, Description: "MacBook Pro 15'", Category: "Tecnologia"}
+	prod.Save()
+	prod.GetAll()
+	fmt.Println(getById(3))
+}	
---
+++ clase3-go/ej2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	ID int
+	Name string
+	DateOfBirth string
+}
+
+type Employee struct {
+	Person		Person
+	ID			int
+	Position	string
+}
+
+func (e Employee) PrintEmployee() {
+	fmt.Printf("%+v \n", e)
+}
+
+func main() {
+	persona := Person{
+		ID: 1234,
+		Name: "Sebastian",
+		DateOfBirth: "28/06/1999",
+	}
+
+	empleado := Employee{
+		Person: persona,
+		ID: 4321,
+		Position: "Software Developer",
+	}
+
+	empleado.PrintEmployee()
+
+
+}
